ea2aa6128fda7db9b997d9803c168632b7aac489
YOGESH <yogrival911@gmail.com>
2021-03-03 13:15:42 +0530

    Assignment

--- app/src/main/java/com/yogdroidtech/assignment/MainActivity.java
+++ app/src/main/java/com/yogdroidtech/assignment/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.yogdroidtech.assignment.response.ItemsResponse;
 
@@ -13,11 +15,27 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView item11, item12, item13, allowedQuant1,selectedQuant1, remainingQuant1, choose;
+    private Spinner spinner11,spinner12, spinner13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        choose = findViewById(R.id.choose);
+
+        item11 = findViewById(R.id.item11);
+        item12 = findViewById(R.id.item12);
+        item13 = findViewById(R.id.item13);
+
+        spinner11 = findViewById(R.id.spinner11);
+        spinner12 = findViewById(R.id.spinner12);
+        spinner13 = findViewById(R.id.spinner13);
+
+        allowedQuant1 = findViewById(R.id.allowedQuant1);
+        selectedQuant1 = findViewById(R.id.selectedQuant1);
+        remainingQuant1 = findViewById(R.id.remainingQuant1);
 
         Retrofit retrofit = RetrofitClient.getInstance();
         RetrofitInterface retrofitInterface = retrofit.create(RetrofitInterface.class);
@@ -27,6 +45,11 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<ItemsResponse> call, Response<ItemsResponse> response) {
                 ItemsResponse itemsResponse = response.body();
                 Log.i("responseyog", response.body().getMessage());
+                choose.setText(response.body().getData().get(0).getName());
+                item11.setText(response.body().getData().get(0).getItemChoice().get(0).getName());
+                item12.setText(response.body().getData().get(0).getItemChoice().get(1).getName());
+                item13.setText(response.body().getData().get(0).getItemChoice().get(2).getName());
+
             }
 
             @Override
